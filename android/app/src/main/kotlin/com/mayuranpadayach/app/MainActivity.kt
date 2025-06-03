db0a42d75e29f5f417e69b82a43c594c24f282eb
Tariku-Islam-Shykat <shykatislam138@gmail.com>
2025-06-03 12:33:54 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/mayuranpadayach/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mayuranpadayach.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:prettyrini/core/controller/theme_controller.dart';
+import 'package:prettyrini/feature/post/ui/post_ui.dart';
+import 'package:prettyrini/route/route.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'core/const/app_colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  configEasyLoading();
+  await SharedPreferences.getInstance();
+  Get.put(ThemeController());
+  runApp(const MyApp());
+}
+
+void configEasyLoading() {
+  EasyLoading.instance
+    ..loadingStyle = EasyLoadingStyle.custom
+    ..backgroundColor = AppColors.grayColor
+    ..textColor = Colors.white
+    ..indicatorColor = Colors.white
+    ..maskColor = Colors.green
+    ..userInteractions = false
+    ..dismissOnTap = false;
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeController themeController = Get.find<ThemeController>();
+
+    return ScreenUtilInit(
+      designSize: const Size(360, 640),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Foot Fitness',
+        getPages: AppRoute.routes,
+        initialRoute: AppRoute.splashScreen,
+        builder: EasyLoading.init(),
+        // home: PostScreen(),
+      ),
+    );
+  }
+}
